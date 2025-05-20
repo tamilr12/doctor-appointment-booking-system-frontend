16420ebce86917c36c11fe790959b721aece1427
tami <you@example.com>
2025-05-20 14:00:32 +0530

    Save changes before pulling remote updates

--- doc_backend-main/index.js
+++ doc_backend-main/index.js
@@ -30,6 +30,7 @@ app.get('*', (req, res) => {
 const allowedOrigins = ['http://localhost:5173'];
 
 
+
 app.use(cors({
   origin: function(origin, callback){
     if(!origin) return callback(null, true);
